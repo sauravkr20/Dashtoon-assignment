4a27df9fd55a9497473473be17548d393fd152cc
sauravkr20 <sauravkr20@iitk.ac.in>
2023-11-27 00:00:14 +0530

    testing & deploying

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<BrowserRouter basename="/Dashtoon-assignment">
+	<BrowserRouter>
 		<AuthContextProvider>
 			<App />
 		</AuthContextProvider>
